b4061e51574a6494b38eb4cd656dc039bfcf6eb4
Alexandra Labusova <sasa.labusova@gmail.com>
2022-11-08 11:33:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+//pages import
+import Home from "./pages/home/Home.jsx";
+import Portfolio from "./pages/portfolio/Portfolio.jsx";
+import AboutMe from "./pages/about/AboutMe.jsx";
+import Contact from "./pages/contact/Contact.jsx";
+import Project from "./pages/project/Project.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <AboutMe />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/projects/:id">
+            <Project />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
